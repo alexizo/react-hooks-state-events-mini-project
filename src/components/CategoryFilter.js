f842a1e5e5876ac2f44b047a145e2692a78c9f69
Alex Mukuha <alex.mukuha@student.moringaschool.com>
2024-08-08 23:27:34 +0300

    react-hooks-state-events-mini-project

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,10 +1,29 @@
 import React from "react";
 
-function CategoryFilter() {
+
+function CategoryFilter({ categories, onCategoryChange }) {
+  const [selectedCategory, setSelectedCategory] = React.useState(categories[0]);
+
+  const handleClick = (category) => {
+    setSelectedCategory(category);
+    if (onCategoryChange) {
+      onCategoryChange(category);
+    }
+  };
+
+
   return (
     <div className="categories">
       <h5>Category filters</h5>
-      {/* render <button> elements for each category here */}
+      {categories.map((category) => (
+        <button 
+          key={category} 
+          className={category === selectedCategory ? "selected" : ""}
+          onClick={() => handleClick(category)}
+        >
+          {category}
+        </button>
+      ))}
     </div>
   );
 }
